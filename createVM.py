c3f5e4d793a525a6cab8e2a398f622144591204f
JexUgaz <a20191417@pucp.edu.pe>
2023-12-11 08:15:46 -0500

    Parámetros añadidos

--- createVM.py
+++ createVM.py
@@ -1,49 +1,63 @@
+import os
 import subprocess,argparse,random
-
-def generar_mac():
-	parte_fija = "fa:16:3e"
-	resto = ':'.join(['{:02x}'.format(random.randint(0, 255)) for _ in range(3)])
-	direccion_mac = parte_fija + ':' + resto
-	return direccion_mac
 
 def runCommand(command):
 	print(command)
-	result=subprocess.run(command,shell=True,text=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	result=subprocess.run("sudo "+command,shell=True,text=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
 def createTap(port,idVlan):
 	runCommand(f"ip tuntap add mode tap name slice{idVlan}-tap{port}")
 	print(f"TAP slice{idVlan}-tap{port}: creado exitosamente!!")
 
-def createVM(port,idVlan):
-	dir_mac=generar_mac()
-	runCommand(f"qemu-system-x86_64 -enable-kvm -vnc 0.0.0.0:{port} -netdev tap,id=slice{idVlan}-tap{port},ifname=slice{idVlan}-tap{port},script=no,downscript=no -device e1000,netdev=slice{idVlan}-tap{port},mac={dir_mac} -daemonize -snapshot cirros-0.5.1-x86_64-disk.img")
+def createVM(port,idVlan,path_imagen,size_ram,dir_mac):
+	set_path_image(path_imagen)
+	runCommand(f"qemu-system-x86_64 -enable-kvm -m {size_ram} -vnc 0.0.0.0:{port} -netdev tap,id=slice{idVlan}-tap{port},ifname=slice{idVlan}-tap{port},script=no,downscript=no -device e1000,netdev=slice{idVlan}-tap{port},mac={dir_mac} -daemonize -snapshot {path_imagen}")
 
+def set_path_image(path_imagen):
+	path_absoluto=os.path.expanduser(path_imagen)
+
+	# Dividir la ruta en el directorio y el nombre del archivo
+	directorio, archivo = os.path.split(path_absoluto)
+
+	# Crear el directorio si no existe
+	if not os.path.exists(directorio):
+		os.makedirs(directorio)
+
+	# Verificar si el archivo ya existe
+	if not os.path.exists(path_absoluto):
+		#Se crea previamente las llaves duplicadas para acceso directo
+		runCommand(f"scp -o StrictHostKeyChecking=no ubuntu@10.0.0.1:{path_absoluto} {path_absoluto}")
 
 if __name__=='__main__':
-	print(generar_mac())
 	parser = argparse.ArgumentParser(description='Script para crear VM')
 	parser.add_argument('param1', type=str, help='Nombre del OVS')
 	parser.add_argument('param2', type=str, help='ID de la Vlan')
 	parser.add_argument('param3', type=str, help='El puerto VNC:')
+	parser.add_argument('param4', type=str, help='El path de la imagen')
+	parser.add_argument('param5', type=str, help='Size Ram de la VM')
+	parser.add_argument('param6', type=str, help='Dirección MAC')
 
 	args = parser.parse_args()
 
 	name_OVS= args.param1
 	id_Vlan= args.param2
 	port_vnc=  args.param3
+	path_imagen=  args.param4
+	size_ram=  args.param5
+	dir_mac= args.param6
 
-        #Creamos el TAP
+    #Creamos el TAP
 	createTap(port_vnc,id_Vlan)
 
         #Creamos la VM
-	createVM(port_vnc,id_Vlan)
+	createVM(port_vnc,id_Vlan,path_imagen,size_ram,dir_mac)
 
         #Añadimos TAP a la OVS
 	runCommand(f"ovs-vsctl add-port {name_OVS} slice{id_Vlan}-tap{port_vnc} tag={id_Vlan}")
 
         #Encendemos el TAP
 	runCommand(f"ip link set dev slice{id_Vlan}-tap{port_vnc} up")
-	
+
 
 
 	
