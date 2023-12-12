4e40d503cebf898e8739b7ff56bb4e2f8fb134cc
JexUgaz <a20191417@pucp.edu.pe>
2023-12-12 00:06:00 -0500

    Remove VMs

---
+++ removeVMs.py
@@ -0,0 +1,30 @@
+import argparse
+import subprocess
+
+def runCommand(command):
+	result=subprocess.run("sudo "+command,shell=True,text=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	return result.stdout.strip()
+
+def kill_qemu_processes(interface_name):
+    # Obtener la lista de PIDs asociados con la interfaz TAP
+    pid = runCommand(f"ps -aux | grep qemu | grep {interface_name} | awk '{{print $2}}'").splitlines()
+    print(f"sudo kill -9 {pid}")
+    #runCommand(f"kill -9 {pid}")
+
+
+def remove_interfaces_with_idVlan(number):
+    # Obtener las interfaces con el número específico
+    result = runCommand(f"ovs-vsctl show | awk '/{number}/ && /Interface/ {{print $2}}'")
+    interfaces = result.splitlines()
+
+    # Iterar sobre las interfaces y eliminar cada una
+    for interface in interfaces:
+        kill_qemu_processes(interface)
+        print(f"sudo ovs-vsctl del-port {interface}")
+        #runCommand(f"ovs-vsctl del-port {interface}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Kill QEMU processes associated with a specific VLAN ID.")
+    parser.add_argument("id_vlan", type=int, help="VLAN ID for which QEMU processes should be killed.")
+    args = parser.parse_args()
+    remove_interfaces_with_idVlan(args.id_vlan)
